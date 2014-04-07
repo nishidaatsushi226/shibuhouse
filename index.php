acf17893320e8c67b35e4f2103d707f80318c02a
YAVAO <oike.masaki@gmail.com>
2014-04-08 05:00:06 +0900

    アバウトの画像のコメントアウトをいじりました

--- index.php
+++ index.php
@@ -101,8 +101,8 @@ wp_reset_query();
             <?php require 'wp-content/themes/shibuhouse/text/what.php' ?>
           </div>
           <a style="margin:0 auto; display: block;" href="./about.php" id="midashi_about"></a>
-          <!--<img style="margin:0 auto; display: block;" src="./img/hatena_midashi.png">-->
-          <img style="margin:0 auto; display: block;" src="<?php bloginfo('template_url'); ?>/img/about_midahsi.png">
+          <img style="margin:0 auto; display: block;" src="./img/hatena_midashi.png">
+          <!--<img style="margin:0 auto; display: block;" src="<?php bloginfo('template_url'); ?>/img/about_midahsi.png">-->
           </div><!-- .inner_midashi -->
         </div><!-- #what -->
 
