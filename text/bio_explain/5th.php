3c34a4f12e3291ef822d8326b247b3e82cd5d3a4
YAVAO <oike.masaki@gmail.com>
2014-04-08 03:53:25 +0900

    Update 5th.php

--- text/bio_explain/5th.php
+++ text/bio_explain/5th.php
@@ -1,8 +1,20 @@
+
+
 <div id="ex_image">
-	<img class="inner_ex_image" src="./img/event_sum/5th/5th_001.jpg">
-	<img class="inner_ex_image" src="./img/event_sum/5th/5th_002.jpg">
-	<img class="inner_ex_image" src="./img/event_sum/5th/5th_003.jpg">
-	<img class="inner_ex_image" src="./img/event_sum/5th/5th_004.jpg">
+<?php $basic_path = "wp-content/themes/shibuhouse/"; ?>
+
+<?php $sum_image = $basic_path.'./img/event_sum/5th/5th_001.jpg'?>
+<img class="inner_ex_image" src="<?php echo $sum_image ?>">
+
+<?php $sum_image = $basic_path.'./img/event_sum/5th/5th_002.jpg'?>
+<img class="inner_ex_image" src="<?php echo $sum_image ?>">
+
+<?php $sum_image = $basic_path.'./img/event_sum/5th/5th_003.jpg'?>
+<img class="inner_ex_image" src="<?php echo $sum_image ?>">
+
+<?php $sum_image = $basic_path.'./img/event_sum/5th/5th_004.jpg'?>
+<img class="inner_ex_image" src="<?php echo $sum_image ?>">
+
 </div>
 <div id="ex_text">
 「渋家5周年パーティー」<br />
