90cbe1b21261ea70a247e3677aa2000edf25fca3
YAVAO <oike.masaki@gmail.com>
2014-04-08 04:16:30 +0900

    Update nation.php

--- text/bio_explain/nation.php
+++ text/bio_explain/nation.php
@@ -1,5 +1,8 @@
+
 <div id="ex_image">
-	<img class="inner_ex_image" src="./img/event_sum/nation/nation_001.jpg">
+<?php $basic_path = "wp-content/themes/shibuhouse/"; ?>
+	<?php $sum_image = $basic_path.'img/event_sum/nation/nation_001.jpg'?>
+	<img class="inner_ex_image" src="<?php echo $sum_image ?>">
 
 </div>
 <div id="ex_text">
