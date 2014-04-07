c1939f8382985d6cfdeaf6c1870e035fbbf0726e
YAVAO <oike.masaki@gmail.com>
2014-04-08 04:22:15 +0900

    Update repub_final.php

--- text/bio_explain/repub_final.php
+++ text/bio_explain/repub_final.php
@@ -1,8 +1,13 @@
 <div id="ex_image">
-	<img class="inner_ex_image" src="./img/event_sum/repub_final/repub_final_001.jpg">
-	<img class="inner_ex_image" src="./img/event_sum/repub_final/repub_final_002.jpg">
-	<img class="inner_ex_image" src="./img/event_sum/repub_final/repub_final_003.jpg">
-	<img class="inner_ex_image" src="./img/event_sum/repub_final/repub_final_004.jpg">
+<?php $basic_path = "wp-content/themes/shibuhouse/"; ?>
+	<?php $sum_image = $basic_path.'img/event_sum/repub_final/repub_final_001.jpg'?>
+	<img class="inner_ex_image" src="<?php echo $sum_image ?>">
+	<?php $sum_image = $basic_path.'img/event_sum/repub_final/repub_final_002.jpg'?>
+	<img class="inner_ex_image" src="<?php echo $sum_image ?>">
+	<?php $sum_image = $basic_path.'img/event_sum/repub_final/repub_final_003.jpg'?>
+	<img class="inner_ex_image" src="<?php echo $sum_image ?>">
+	<?php $sum_image = $basic_path.'img/event_sum/repub_final/repub_final_004.jpg'?>
+	<img class="inner_ex_image" src="<?php echo $sum_image ?>">
 </div>
 <div id="ex_text">
 「REPUBLIC VOL.10 - THE FINAL -」<br />
