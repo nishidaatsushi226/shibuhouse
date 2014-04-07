306b82883776de44931ccfaf81915d75cd7c1c8d
YAVAO <oike.masaki@gmail.com>
2014-04-08 04:10:58 +0900

    Update gaget_1.php

--- text/bio_explain/gaget_1.php
+++ text/bio_explain/gaget_1.php
@@ -1,5 +1,8 @@
+
 <div id="ex_image">
-	<img class="inner_ex_image" src="./img/media_sum/gaget_1/gajet_1_001.jpg">
+<?php $basic_path = "wp-content/themes/shibuhouse/"; ?>
+	<?php $sum_image = $basic_path.'img/media_sum/gaget_1/gajet_1_001.jpg'?>
+	<img class="inner_ex_image" src="<?php echo $sum_image ?>">
 </div>
 <div id="ex_text">
 「ガジェット通信」<br />
