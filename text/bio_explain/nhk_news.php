51e2349bf98065e977f2bfdb67ef21277297e557
YAVAO <oike.masaki@gmail.com>
2014-04-08 04:17:50 +0900

    Update nhk_news.php

--- text/bio_explain/nhk_news.php
+++ text/bio_explain/nhk_news.php
@@ -1,5 +1,8 @@
+
 <div id="ex_image">
-	<img class="inner_ex_image" src="./img/media_sum/nhk_news/nhk_news_001.jpg">
+<?php $basic_path = "wp-content/themes/shibuhouse/"; ?>
+	<?php $sum_image = $basic_path.'img/media_sum/nhk_news/nhk_news_001.jpg'?>
+	<img class="inner_ex_image" src="<?php echo $sum_image ?>">
 </div>
 <div id="ex_text">
 「NHK ニュース7」(NHK)<br />
