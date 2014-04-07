98893fd811f8af8fea274d3ae6091d75bc2aa557
yukiishida <amusing365@gmail.com>
2014-04-08 08:06:04 +0900

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,9 +1,9 @@
     <footer>
       <hr style="">
       <div id="footer_table">
-      <img src="banner/komekome_banner.png">
-      <img src="banner/dumdum_banner.png">
-      <img src="banner/shibumaga_banner.png">
+      <a href="/komekome/"><img src="banner/komekome_banner.png"></a>
+      <a href="https://www.youtube.com/watch?v=2M-rA4aO0BY" target="_blank"><img src="banner/dumdum_banner.png"></a>
+      <a href="/add_shibumaga/"><img src="banner/shibumaga_banner.png"></a>
         </table>
       </div><!-- #footer_tale -->
       <!--<img src="<?php bloginfo('template_url'); ?>/img/footer_underline.png">-->
