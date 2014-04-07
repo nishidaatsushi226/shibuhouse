3b6166c5a7230a71d0f5448b3b34f0a69c2e4261
YAVAO <oike.masaki@gmail.com>
2014-04-08 04:23:03 +0900

    Update rin.php

--- text/bio_explain/rin.php
+++ text/bio_explain/rin.php
@@ -1,7 +1,11 @@
 <div id="ex_image">
-	<img class="inner_ex_image" src="./img/exhibition_sum/rin/rin_001.jpg">
-	<img class="inner_ex_image" src="./img/exhibition_sum/rin/rin_003.jpg">
-	<img class="inner_ex_image" src="./img/exhibition_sum/rin/rin_002.jpg">
+<?php $basic_path = "wp-content/themes/shibuhouse/"; ?>
+	<?php $sum_image = $basic_path.'img/exhibition_sum/rin/rin_001.jpg'?>
+	<img class="inner_ex_image" src="<?php echo $sum_image ?>">
+	<?php $sum_image = $basic_path.'img/exhibition_sum/rin/rin_003.jpg'?>
+	<img class="inner_ex_image" src="<?php echo $sum_image ?>">
+	<?php $sum_image = $basic_path.'img/exhibition_sum/rin/rin_002.jpg'?>
+	<img class="inner_ex_image" src="<?php echo $sum_image ?>">
 </div>
 <div id="ex_text">
 「リンカイ展」<br />
