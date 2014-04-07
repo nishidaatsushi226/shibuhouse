07989077750f1446c78ea963f62efc6c226489c9
YAVAO <oike.masaki@gmail.com>
2014-04-08 04:11:58 +0900

    Update gig.php

--- text/bio_explain/gig.php
+++ text/bio_explain/gig.php
@@ -1,7 +1,12 @@
+
 <div id="ex_image">
-	<img class="inner_ex_image" src="./img/exhibition_sum/gig/gig_001.jpg">
-	<img class="inner_ex_image" src="./img/exhibition_sum/gig/gig_002.jpg">
-	<img class="inner_ex_image" src="./img/exhibition_sum/gig/gig_003.jpg">
+<?php $basic_path = "wp-content/themes/shibuhouse/"; ?>
+	<?php $sum_image = $basic_path.'img/exhibition_sum/gig/gig_001.jpg'?>
+	<img class="inner_ex_image" src="<?php echo $sum_image ?>">
+	<?php $sum_image = $basic_path.'img/exhibition_sum/gig/gig_002.jpg'?>
+	<img class="inner_ex_image" src="<?php echo $sum_image ?>">
+	<?php $sum_image = $basic_path.'img/exhibition_sum/gig/gig_003.jpg'?>
+	<img class="inner_ex_image" src="<?php echo $sum_image ?>">
 </div>
 <div id="ex_text">
 「ArtGigTokyo2 - 怖いよ、ママー! -」<br />
