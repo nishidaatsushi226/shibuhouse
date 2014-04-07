29735f6df247c575833045eb0df49f68d5600e6a
yukiishida <amusing365@gmail.com>
2014-04-08 07:54:38 +0900

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,6 +1,9 @@
     <footer>
       <hr style="">
       <div id="footer_table">
+      <img src="banner/komekome_banner.png">
+      <img src="banner/dumdum_banner.png">
+      <img src="banner/shibumaga_banner.png">
         </table>
       </div><!-- #footer_tale -->
       <!--<img src="<?php bloginfo('template_url'); ?>/img/footer_underline.png">-->
