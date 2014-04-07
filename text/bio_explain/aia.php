089d3d0bf9a528465ed975e39d49eaf48e3b0502
YAVAO <oike.masaki@gmail.com>
2014-04-08 03:56:50 +0900

    Update aia.php

--- text/bio_explain/aia.php
+++ text/bio_explain/aia.php
@@ -1,5 +1,9 @@
 <div id="ex_image">
-	<img class="inner_ex_image" src="./img/media_sum/aia/aia_001.jpg">
+<?php $basic_path = "wp-content/themes/shibuhouse/"; ?>
+
+<?php $sum_image = $basic_path.'./img/media_sum/aia/aia_001.jpg'?>
+<img class="inner_ex_image" src="<?php echo $sum_image ?>">
+
 </div>
 <div id="ex_text">
 「Art it asia」<br />
