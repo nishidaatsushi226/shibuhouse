cccf64c222e7877ce788d2bac689ebf345f919f7
YAVAO <oike.masaki@gmail.com>
2014-04-08 04:27:48 +0900

    Update tss.php

--- text/bio_explain/tss.php
+++ text/bio_explain/tss.php
@@ -1,5 +1,7 @@
 <div id="ex_image">
-	<img class="inner_ex_image" src="./img/media_sum/tss/tss_001.jpg">
+<?php $basic_path = "wp-content/themes/shibuhouse/"; ?>
+	<?php $sum_image = $basic_path.'img/media_sum/tss/tss_001.jpg'?>
+	<img class="inner_ex_image" src="<?php echo $sum_image ?>">
 </div>
 <div id="ex_text">
 『地域社会圏主義』[山本理顕/LIXIL出版] <br />
