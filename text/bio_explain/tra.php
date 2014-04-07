1ce473060d7c1c557ffab567b08ab0b3c4ab10ac
YAVAO <oike.masaki@gmail.com>
2014-04-08 04:26:03 +0900

    Update tra.php

--- text/bio_explain/tra.php
+++ text/bio_explain/tra.php
@@ -1,8 +1,14 @@
+
 <div id="ex_image">
-	<img class="inner_ex_image" src="./img/exhibition_sum/tra/tra_001.jpg">
-	<img class="inner_ex_image" src="./img/exhibition_sum/tra/tra_002.jpg">
-	<img class="inner_ex_image" src="./img/exhibition_sum/tra/tra_003.jpg">
-	<img class="inner_ex_image" src="./img/exhibition_sum/tra/tra_004.jpg">
+<?php $basic_path = "wp-content/themes/shibuhouse/"; ?>
+	<?php $sum_image = $basic_path.'img/exhibition_sum/tra/tra_001.jpg'?>
+	<img class="inner_ex_image" src="<?php echo $sum_image ?>">
+	<?php $sum_image = $basic_path.'img/exhibition_sum/tra/tra_002.jpg'?>
+	<img class="inner_ex_image" src="<?php echo $sum_image ?>">
+	<?php $sum_image = $basic_path.'img/exhibition_sum/tra/tra_003.jpg'?>
+	<img class="inner_ex_image" src="<?php echo $sum_image ?>">
+	<?php $sum_image = $basic_path.'img/exhibition_sum/tra/tra_004.jpg'?>
+	<img class="inner_ex_image" src="<?php echo $sum_image ?>">
 </div>
 <div id="ex_text">
 「TRANS ARTS TOKYO」<br />
