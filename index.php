ac86d6bf731515cd5f6cdb4de8266c80b724e7e2
ayafuji / Yuki Takada <ayafuji.aya@gmail.com>
2014-04-08 15:22:56 +0900

    Update index.php

--- index.php
+++ index.php
@@ -95,7 +95,7 @@ if ($date_interval > 0) {
 <sep class="topics_date">[<?php echo $art_event_media ?>] 
 <a href="<?php echo $hottopics_link ?>" target="_blank">
 <?php echo $hottopics_date ?>（<?php echo $week[$w] ?>）
-<?php if($hottopics_date_stop !== "") echo $hottopics_date_stop ?>
+<?php if($hottopics_date_stop !== "") echo "から".$hottopics_date_stop."（".$week[$w_stop]."）　" ?>
 <?php echo $hottopics_text ?>
 </a></sep></li>
 <?php
