d1d85db757cd339055287f484a5ddd87454d909a
ayafuji / Yuki Takada <ayafuji.aya@gmail.com>
2014-04-08 12:49:07 +0900

    Update media.php

--- text/bio/media.php
+++ text/bio/media.php
@@ -14,72 +14,72 @@
 </tr>
 <tr>
 <th>2013.03</th>
-<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/art_it_asia.php");'><div id="bio_background">「Art it asia」/「売り渋家」</div></td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("<?php bloginfo('template_url'); ?>/text/bio_explain/art_it_asia.php");'><div id="bio_background">「Art it asia」/「売り渋家」</div></td>
 </tr>
 <tr>
 <th>2013.03</th>
-<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/25d_news.php");'><div id="bio_background">「2.5D」/「「渋家」が新メンバー募集、新料金は 2.5 万」</div></td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("<?php bloginfo('template_url'); ?>/text/bio_explain/25d_news.php");'><div id="bio_background">「2.5D」/「「渋家」が新メンバー募集、新料金は 2.5 万」</div></td>
 </tr>
 <tr>
 <th>2013.03</th>
-<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/nhk_news.php");'><div id="bio_background">「NHKニュース7」[NHK]/「アートフェア東京」</div></td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("<?php bloginfo('template_url'); ?>/text/bio_explain/nhk_news.php");'><div id="bio_background">「NHKニュース7」[NHK]/「アートフェア東京」</div></td>
 </tr>
 <tr>
 <th>2013.01</th>
-<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/asahi_2013.php");'><div id="bio_background">『朝日新聞』/「ひとつ屋根の下、飛躍もシェア」</div></td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("<?php bloginfo('template_url'); ?>/text/bio_explain/asahi_2013.php");'><div id="bio_background">『朝日新聞』/「ひとつ屋根の下、飛躍もシェア」</div></td>
 </tr>
 <tr>
 <th>2012.09</th>
-<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/netokaru_1.php");'><div id="bio_background">「NETOKARU」/「“渋家”新代表に菅井早苗が就任」</div></td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("<?php bloginfo('template_url'); ?>/text/bio_explain/netokaru_1.php");'><div id="bio_background">「NETOKARU」/「“渋家”新代表に菅井早苗が就任」</div></td>
 </tr>
 <tr>
 <th>2012.08</th>
-<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/aia.php");'><div id="bio_background">「Art it asia」/「渋家」</div></td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("<?php bloginfo('template_url'); ?>/text/bio_explain/aia.php");'><div id="bio_background">「Art it asia」/「渋家」</div></td>
 </tr>
 <tr>
 <th>2012.07</th>
-<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/bbc.php");'><div id="bio_background">「Japan, Fall of the Rising Sun」[BBC Three]</div></td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("<?php bloginfo('template_url'); ?>/text/bio_explain/bbc.php");'><div id="bio_background">「Japan, Fall of the Rising Sun」[BBC Three]</div></td>
 </tr>
 <tr>
 <th>2012.06</th>
-<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/endan.php");'><div id="bio_background">「#エンダン W」[NOTTV]</div></td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("<?php bloginfo('template_url'); ?>/text/bio_explain/endan.php");'><div id="bio_background">「#エンダン W」[NOTTV]</div></td>
 </tr>
 
 <tr>
 <th>2012.06</th>
-<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/tennen.php");'><div id="bio_background">『天然生活』/「物々交換を開催してみました」</div></td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("<?php bloginfo('template_url'); ?>/text/bio_explain/tennen.php");'><div id="bio_background">『天然生活』/「物々交換を開催してみました」</div></td>
 </tr>
 
 <tr>
 <th>2012.03</th>
-<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/artguide.php");'><div id="bio_background">『東京アートガイド』[美術手帖編集部]</div></td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("<?php bloginfo('template_url'); ?>/text/bio_explain/artguide.php");'><div id="bio_background">『東京アートガイド』[美術手帖編集部]</div></td>
 </tr>
 <tr>
 <th>2012.01</th>
-<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/tss.php");'><div id="bio_background">『地域社会圏主義』[山本理顕]</div></td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("<?php bloginfo('template_url'); ?>/text/bio_explain/tss.php");'><div id="bio_background">『地域社会圏主義』[山本理顕]</div></td>
 </tr>
 <tr>
 <th>2011.12</th>
-<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/dommune.php");'><div id="bio_background">『DOMMUNE オフィシャルガイドブック 2』[DOMMUNE]</div></td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("<?php bloginfo('template_url'); ?>/text/bio_explain/dommune.php");'><div id="bio_background">『DOMMUNE オフィシャルガイドブック 2』[DOMMUNE]</div></td>
 </tr>
 <tr>
 <th>2011.11</th>
-<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/hk.php");'><div id="bio_background">『HK 2』/「特集 さあ、〈一緒に〉暮そう」</div></td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("<?php bloginfo('template_url'); ?>/text/bio_explain/hk.php");'><div id="bio_background">『HK 2』/「特集 さあ、〈一緒に〉暮そう」</div></td>
 </tr>
 <tr>
 <th>2011.11</th>
-<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/spa_1.php");'><div id="bio_background">『SPA!』/「渋谷で家賃 3 万円の快適シェアハウス」</div></td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("<?php bloginfo('template_url'); ?>/text/bio_explain/spa_1.php");'><div id="bio_background">『SPA!』/「渋谷で家賃 3 万円の快適シェアハウス」</div></td>
 </tr>
 <tr>
 <th>2011.08</th>
-<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/gaget_1.php");'><div id="bio_background">「ガジェット通信」/「シェアハウス”渋家”の過去・現在・未来」</div></td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("<?php bloginfo('template_url'); ?>/text/bio_explain/gaget_1.php");'><div id="bio_background">「ガジェット通信」/「シェアハウス”渋家”の過去・現在・未来」</div></td>
 </tr>
 <tr>
 <th>2011.05</th>
-<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/twe.php");'><div id="bio_background">「Tokyo Weekender」/「Uncovering a Tokyo discovery」</div></td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("<?php bloginfo('template_url'); ?>/text/bio_explain/twe.php");'><div id="bio_background">「Tokyo Weekender」/「Uncovering a Tokyo discovery」</div></td>
 </tr>
 <tr>
 <th>2011.03</th>
-<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/asahi_j.php");'><div id="bio_background">『朝日ジャーナル』/「特集 日本破壊計画 – シェアハウスの人々 -」</div></td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("<?php bloginfo('template_url'); ?>/text/bio_explain/asahi_j.php");'><div id="bio_background">『朝日ジャーナル』/「特集 日本破壊計画 – シェアハウスの人々 -」</div></td>
 </tr>
 </table>
